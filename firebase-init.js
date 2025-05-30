172f07921c81641a5e3c12f893a7abecb9c611a7
Milo455 <camiloarbelaez455@gmail.com>
2025-05-30 12:43:10 -0700

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,13 +1,13 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
+import { getFirestore }    from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { getStorage }      from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
 import { getAuth, signInAnonymously } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBFpO3mzD94Wa_oCywdzHUaWJONtHugTuE",
   authDomain: "mis-sucus.firebaseapp.com",
   projectId: "mis-sucus",
-  storageBucket: "mis-sucus.appspot.com",
+  storageBucket: "mis-sucus.firebasestorage.app",
   messagingSenderId: "535386004336",
   appId: "1:535386004336:web:4701b88dc0ed7f75164db5"
 };
@@ -16,8 +16,9 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const storage = getStorage(app);
 const auth = getAuth(app);
+
 signInAnonymously(auth).catch(console.error);
 
-// Exponemos para app.js
 window.db = db;
 window.storage = storage;
+window.auth = auth;
