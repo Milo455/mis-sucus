41e405a5b444935c0f566ccf7be80d9a74e3343c
Milo455 <camiloarbelaez455@gmail.com>
2025-05-30 14:24:30 -0700

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,5 +1,4 @@
 // firebase-init.js
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getFirestore }    from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
@@ -12,6 +11,5 @@ const firebaseConfig = {
   appId: "1:535386004336:web:4701b88dc0ed7f75164db5"
 };
 
-// Inicializar Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
