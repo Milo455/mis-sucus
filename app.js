43be52fd97cce91fc8c75d25d7012ac655d4cff5
Milo455 <camiloarbelaez455@gmail.com>
2025-05-30 18:25:31 -0700

    Update app.js

--- app.js
+++ app.js
@@ -180,18 +180,16 @@ document.getElementById('save-event').addEventListener('click', async () => {
     }
   });
 
-// Mostrar modal al hacer clic en el botón flotante
-document.getElementById('open-event-modal').addEventListener('click', () => {
-  document.getElementById('event-modal').style.display = 'block';
+// Abrir modal de agregar evento
+document.getElementById('open-add-event-modal').addEventListener('click', () => {
+  document.getElementById('add-event-modal').style.display = 'block';
 });
 
-// Cerrar modal al hacer clic fuera del contenido
-window.addEventListener('click', (event) => {
-  const modal = document.getElementById('event-modal');
-  if (event.target === modal) {
-    modal.style.display = 'none';
-  }
+// Cerrar modal de agregar evento
+document.getElementById('close-add-event').addEventListener('click', () => {
+  document.getElementById('add-event-modal').style.display = 'none';
 });
+
 
   // Cerrar modal Calendario
   btnCloseCalendar.addEventListener('click', () => {
